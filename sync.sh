c6c3302ed66a461482db844a69a1313dfe023bed
zerohedgePodcast <zerohedgePodcast@outlook.com>
2022-02-08 15:01:16 +0000

    msg auto

--- sync.sh
+++ sync.sh
@@ -14,7 +14,7 @@ fi
 # If a command fails then the deploy stops
 set -e
 if [[ `git status --porcelain` ]]; then
-	printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+	printf "\033[0;32mDeploying updates to GitHub B)\033[0m\n"
 
 	# Add changes to git
 	git add -A
@@ -29,5 +29,5 @@ if [[ `git status --porcelain` ]]; then
 	# Push source
 	git push
 else
-	printf "\033[0;32mNo changes to deploy...\033[0m\n"
+	printf "\033[0;32mNo changes to deploy ;)\033[0m\n"
 fi
